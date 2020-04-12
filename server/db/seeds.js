267e600c2298eca5018d42aa34b153b5ec6ce76e
Rob Mathews <robbis@gmail.com>
2020-04-12 14:48:12 +0100

    added server functionality

---
+++ server/db/seeds.js
@@ -0,0 +1,13 @@
+use food_app;
+
+db.dropDatabase();
+
+
+db.favourites.insertMany([
+{
+recipe_uri: "recipe_0f4bb9f24d21fa3e064b3f66083d954c"
+},
+{
+recipe_uri: "recipe_322a233df2b6d8a72d301472fdf0f024"
+}
+])
