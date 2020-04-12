36737890f4a350dea4822adc11e46f381647d1af
Rob Mathews <robbis@gmail.com>
2020-04-12 15:01:22 +0100

    updated database layout

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -5,9 +5,11 @@ db.dropDatabase();
 
 db.favourites.insertMany([
 {
-recipe_uri: "recipe_0f4bb9f24d21fa3e064b3f66083d954c"
+    name: "Egg drop chicken noodle soup",
+    recipe_uri: "recipe_0f4bb9f24d21fa3e064b3f66083d954c"
 },
 {
-recipe_uri: "recipe_322a233df2b6d8a72d301472fdf0f024"
+    name: "Chicken, Vegetable, and Ricotta Frittata Recipe",
+    recipe_uri: "recipe_322a233df2b6d8a72d301472fdf0f024"
 }
 ])
